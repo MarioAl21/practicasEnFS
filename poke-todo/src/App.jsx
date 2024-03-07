2c311fdae70cbd1a8a66ac57fd5f14385bc4d7ca
MarioAl21 <mario.nunez@alumno.buap.mx>
2024-03-06 20:25:18 -0600

    Feat: Add the table & paginatio to my Pokemon Capture App V1

---
+++ poke-todo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Poketable } from './components/Poketable';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Pokemon Capture App</h1>
+     <Poketable />
+    </>
+  )
+}
+
+export default App
